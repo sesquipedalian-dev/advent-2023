79701358817d9f8576fce9df3a0406204ab9d81b
Scott Agnew <sesquipedalian.dev@gmail.com>
2023-12-07 06:56:00 -0700

    don't try every other symbol

--- day7/day7.rb
+++ day7/day7.rb
@@ -36,7 +36,8 @@ SYMBOL_COUNTS_PRIORITY = [
     [1, 1, 1, 1, 1] # weakest
 ]
 
-def symbols_to_type_score(symbols)
+
+def symbols_to_type_score(symbols, j_count=0)
     # Five of a kind, where all five cards have the same label: AAAAA
     # Four of a kind, where four cards have the same label and one card has a different label: AA8AA
     # Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
@@ -44,33 +45,25 @@ def symbols_to_type_score(symbols)
     # Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
     # One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
     # High card, where all cards' labels are distinct: 23456
+    #
+    # if you have wildcards (j_count), it's always best to use them as copies of your most frequent symbol
 
     counts = symbols.inject({}) do |accum, s| 
         accum.merge({ s => (accum[s] || 0) + 1 })
     end
     counts = counts.values.sort.reverse
+    counts << 0 if counts.empty?
+    
+    counts[0] += j_count
     SYMBOL_COUNTS_PRIORITY.index(counts)
 end
 
 def symbols_to_type_score_part_2(symbols)
     # same as p1 unless 'J' is present, then we try all the variations and pick the strongest
 
-    first_index_of_j = symbols.join.index('J')
-
-    if first_index_of_j
-        before = symbols[0...first_index_of_j]
-        after = symbols[(first_index_of_j + 1)..]
-        min_rank = 1000
-        SYMBOL_PRIORITY_PART_2.each do |sym|
-            next if sym == 'J'
-
-            new_min = symbols_to_type_score_part_2([before, sym, after].flatten)
-            min_rank = [min_rank, new_min].min
-        end
-        min_rank
-    else
-        symbols_to_type_score(symbols)
-    end
+    symbols = symbols.join
+    j_count = symbols.size - (symbols.gsub!('J', '')&.size || symbols.size)
+    symbols_to_type_score(symbols.split('') || [], j_count || 0)
 end
 
 def parse(input, part_2 = false)
@@ -145,8 +138,9 @@ hands = parse(input)
 # hands.each do |h|
 #     puts "hand #{h[SYMBOLS].join}, #{h[BID]} #{h[RANK]}"
 # end
+# 253933213
 puts part_1(hands)
 
 hands = parse(input, true)
+# 253473930
 puts part_1(hands, SYMBOL_PRIORITY_PART_2)
-253473930
